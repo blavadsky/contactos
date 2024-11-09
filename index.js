8b7fb51e849b37d3094103ca03027062efcd4221
esneider.calvo <esneider.calvo@utp.edu.co>
2024-11-09 00:14:26 +0000

    "cors"

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
-import express from "express"
+import express from "express";
 import { createClient } from "@libsql/client";
-import mysql from 'mysql2/promise';
+import cors from 'cors';
 
-const app = express()
-const port = parseInt(process.env.PORT) || 3000;
+const app = express();
+const port = parseInt(process.env.PORT) || 4200;
 
-app.use(express.json());
-
-const TURSO_DATABASE_URL="libsql://dbweb-blavadsky.turso.io"
+const TURSO_DATABASE_URL="libsql://dbweb-blavadsky.turso.io";
 const TURSO_AUTH_TOKEN="eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJnaWQiOiI5Zjc3NTFhNi1jYTZmLTQ2MzItYTgxYS1hZTc0NGNiNmVjYTEiLCJpYXQiOjE3MjkzMDUxNjV9.VotboVZbauNsLgHLyk7OswVjYoVvrgxuUIM2BL6jsVMT7f72AaM7sUlqm6YXbS0-yThNrf6IWwiv_5LMIZovCg"
+
 
 if (!TURSO_AUTH_TOKEN || !TURSO_DATABASE_URL) {
   throw new Error("TURSO_AUTH_TOKEN and TURSO_DATABASE_URL must be set");
 }
-//const { TURSO_AUTH_TOKEN ="libsql://dbweb-blavadsky.turso.io", TURSO_DATABASE_URL ="eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJnaWQiOiI5Zjc3NTFhNi1jYTZmLTQ2MzItYTgxYS1hZTc0NGNiNmVjYTEiLCJpYXQiOjE3MjkzMDUxNjV9.VotboVZbauNsLgHLyk7OswVjYoVvrgxuUIM2BL6jsVMT7f72AaM7sUlqm6YXbS0-yThNrf6IWwiv_5LMIZovCg" } = process.env;
+
 
 const turso = createClient({
   url: TURSO_DATABASE_URL,
   authToken: TURSO_AUTH_TOKEN,
 });
+
+
+const corsOptions = {
+  origin: 'http://localhost:4200/',
+  methods: ['GET', 'POST', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+
+app.use(express.json());
+app.use(cors(corsOptions));
+
+
+
+//const { TURSO_AUTH_TOKEN ="libsql://dbweb-blavadsky.turso.io", TURSO_DATABASE_URL ="eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJnaWQiOiI5Zjc3NTFhNi1jYTZmLTQ2MzItYTgxYS1hZTc0NGNiNmVjYTEiLCJpYXQiOjE3MjkzMDUxNjV9.VotboVZbauNsLgHLyk7OswVjYoVvrgxuUIM2BL6jsVMT7f72AaM7sUlqm6YXbS0-yThNrf6IWwiv_5LMIZovCg" } = process.env;
+
 
 app.get('/', async(req, res) => {
   const ans2 = await turso.execute(`SELECT * FROM contacts`);
